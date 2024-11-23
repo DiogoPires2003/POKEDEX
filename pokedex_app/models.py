10d18ffd53fd3d4742f0269b1bee516f75a159d9
Diogo Filipe Alves Pires <47152602+DFALVESP@users.noreply.github.com>
2024-11-23 13:34:28 +0100

    models update

--- pokedex_app/models.py
+++ pokedex_app/models.py
@@ -1,3 +1,20 @@
+from xmlrpc.client import MAXINT
+
 from django.db import models
 
 # Create your models here.
+class pokemon(models.Model):
+    id = models.IntegerField(MAXINT = 153)
+    name= models.CharField()
+    cries = models.CharField()
+    height = models.CharField()
+    weight = models.IntegerField()
+    location = models.CharField()
+    image = models.CharField()
+
+class abilities(models.Model):
+    ability_name = models.CharField()
+    ability_url = models.CharField()
+    hidden = models.BooleanField()
+    slot = models.IntegerField()
+

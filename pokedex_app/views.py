1a510a6a8237e91e89ae46cd0756761fa8169e7d
Diogo Filipe Alves Pires <47152602+DFALVESP@users.noreply.github.com>
2024-11-24 06:59:44 +0100

    resest whos that pokemon

--- pokedex_app/views.py
+++ pokedex_app/views.py
@@ -213,7 +213,7 @@ def detail(request, poke_id):
     return render(request, 'home/info.html', {'pokemon': pokemon})
 
 def whoIsThatPokemon(request):
-    random_id = random.randint(1,153)
+    random_id = random.randint(1,151)
     api_url = f"{BASE_URL}/pokemons/{random_id}"
 
     respons = requests.get(api_url)
